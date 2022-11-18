225ba1505fb17dc955d0cfa14e578081649c42ea
Sankalp Tiwari <111833789+tiwarisankalp@users.noreply.github.com>
2022-11-17 19:08:01 -0800

    Created Logic LiveScoreObserver

--- Java-Space-Invaders-master/src/com/zetcode/LiveScoreObserver.java
+++ Java-Space-Invaders-master/src/com/zetcode/LiveScoreObserver.java
@@ -1,2 +1,30 @@
-package com.zetcode;public class LiveScoreObserver {
+package com.zetcode;
+
+import java.awt.Graphics;
+
+public class LiveScoreObserver implements ILiveScoreObserver {
+
+    private int score;
+
+    public LiveScoreObserver(ILiveScoreSubject liveScore){
+        liveScore.registerObserver(this);
+    }
+
+    @Override
+    public void update(int score) {
+        this.score = score;
+        displayCurrent();
+    }
+
+    public void update(int score, Graphics g) {
+        this.score = score;
+        drawScore(score, g);
+    }
+
+    public void displayCurrent(){
+    }
+
+    public void drawScore(int currentScore, Graphics g) {
+        g.drawString("Score: "+ currentScore , Commons.BOARD_WIDTH - 90, 20);
+    }
 }
