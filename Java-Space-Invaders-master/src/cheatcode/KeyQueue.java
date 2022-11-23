4a98f1463b6e1b71f4f0fd760557392553143a39
Zaber Ahmed <zaber.ahmed@gmail.com>
2022-11-23 12:19:15 -0800

    Key Logger works continues.

---
+++ Java-Space-Invaders-master/src/cheatcode/KeyQueue.java
@@ -0,0 +1,35 @@
+package cheatcode;
+
+public class KeyQueue {
+
+    static StringBuffer keyLogger = new StringBuffer();
+    private static KeyQueue instance = null;
+    
+    private KeyQueue() {
+    	
+    }
+    
+    public static KeyQueue getInstance() {
+        if (instance == null) {
+        	instance = new KeyQueue();
+        }
+        return instance;
+    }
+
+    public synchronized void add(String value) {
+    	keyLogger.append(value);
+    	if(keyLogger.toString().toLowerCase().contains("bomb")) {
+    		reset();    		
+    	} else if(keyLogger.toString().toLowerCase().contains("reset")) {
+    		reset();
+    	} else if(keyLogger.toString().toLowerCase().contains("007")) {
+    		reset();
+    	} else if(keyLogger.toString().toLowerCase().contains("music")) {
+    		reset();
+    	}
+    }
+
+    private void reset() {
+    	keyLogger = new StringBuffer();
+    }
+}
