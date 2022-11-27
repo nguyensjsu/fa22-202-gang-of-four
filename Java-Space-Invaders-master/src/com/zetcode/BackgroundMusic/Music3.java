9a758ceb3939e0a114b165879da021dc7c689234
Sankalp Tiwari <111833789+tiwarisankalp@users.noreply.github.com>
2022-11-26 22:12:45 -0800

    Added music files to source resources and updated source paths

--- Java-Space-Invaders-master/src/com/zetcode/BackgroundMusic/Music3.java
+++ Java-Space-Invaders-master/src/com/zetcode/BackgroundMusic/Music3.java
@@ -3,6 +3,6 @@ package com.zetcode.BackgroundMusic;
 public class Music3 implements IMusicStrategy{
     @Override
     public void runMusic() {
-        SoundHandler.runMusic("/Users/sankalptiwari/Downloads/Background MusicJava-Space-Invaders-master 3/res/music3.wav");
+        SoundHandler.runMusic("resources/music3.wav");
     }
 }
