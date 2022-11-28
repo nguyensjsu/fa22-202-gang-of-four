fe971880e931a78b3db1d8f0760eb7cf0c817c9c
Harshit Chouksey <harshit.chouksey@sjsu.edu>
2022-11-27 22:24:48 -0800

    Multiple lives change 1

---
+++ Java-Space-Invaders-master/src/com/zetcode/RemainingLivesSubject.java
@@ -0,0 +1,63 @@
+package com.zetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.awt.Graphics;
+
+public class RemainingLivesSubject implements IRemainingLivesSubject{
+
+    private List<RemainingLivesObserver> observers;
+
+    private int lives;
+
+    public RemainingLivesSubject(int lives)
+    {
+        this.lives = lives;
+        this.observers = new ArrayList<>();
+    }
+    @Override
+    public void add(RemainingLivesObserver o) {
+
+        observers.add(o);
+
+
+    }
+
+    @Override
+    public void remove(RemainingLivesObserver o) {
+        int observerIndex = observers.indexOf(o);
+        if (observerIndex >= 0) {
+            observers.remove(observerIndex);
+        }
+    }
+
+
+
+    @Override
+    public void notifyObservers() {
+        observers.forEach(o -> { o.update(lives);});
+    }
+
+    @Override
+    public void notifyObservers(Graphics g) {
+        observers.forEach(o -> { o.update(lives,g);});
+    }
+
+    public int reduceLives(int lives)
+    {
+        this.lives = lives -1 ;
+        notifyObservers();
+        return  this.lives;
+    }
+
+    public void drawLives(int currLives, Graphics g)
+    {
+        notifyObservers(g);
+    }
+
+    public int liveStatus()
+    {
+        return  this.lives;
+    }
+
+}
