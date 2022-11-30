f21e5a9e43faed92b1b57470a508525321113ecc
Sankalp Tiwari <111833789+tiwarisankalp@users.noreply.github.com>
2022-11-30 12:47:35 -0800

    Updated Concrete Strategy Music2 Class

--- Java-Space-Invaders-master/src/com/zetcode/BackgroundMusic/Music2.java
+++ Java-Space-Invaders-master/src/com/zetcode/BackgroundMusic/Music2.java
@@ -1,8 +1,17 @@
 package com.zetcode.BackgroundMusic;
 
+import javax.sound.sampled.Clip;
+
 public class Music2 implements IMusicStrategy{
+    private Clip cp;
     @Override
     public void runMusic() {
-        SoundHandler.runMusic("resources/music2.wav");
+        cp = SoundHandler.runMusic("resources/music2.wav");
+    }
+    public void closeMusic() {
+        SoundHandler.closeMusic(cp);
+    }
+    public String toString() {
+        return "music2";
     }
 }
