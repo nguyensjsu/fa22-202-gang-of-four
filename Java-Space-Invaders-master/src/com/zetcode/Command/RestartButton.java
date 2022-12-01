1b6ecf8c25e4c947fd5206c48eec8018c29f5a00
Sankalp Tiwari <111833789+tiwarisankalp@users.noreply.github.com>
2022-12-01 13:14:38 -0800

    Added Receiver  class for Command Pattern - Restart

---
+++ Java-Space-Invaders-master/src/com/zetcode/Command/RestartButton.java
@@ -0,0 +1,31 @@
+package com.zetcode.Command;
+
+import com.zetcode.SpaceInvaders;
+
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import java.io.IOException;
+
+import static com.zetcode.Board.playMusic;
+import static com.zetcode.Board.stopMusic;
+
+//Receiver
+public class RestartButton {
+    public void pressButton(){
+        var ex = new SpaceInvaders();
+        ex.setVisible(true);
+        try {
+            stopMusic();
+            playMusic();
+        } catch (UnsupportedAudioFileException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (LineUnavailableException e) {
+            throw new RuntimeException(e);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
