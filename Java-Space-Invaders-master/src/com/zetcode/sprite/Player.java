e376ac916a99360f9bc620074fe53a9203f514cc
Harshit Chouksey <harshit.chouksey@sjsu.edu>
2022-12-03 01:28:12 -0800

    Repositioning player start point

--- Java-Space-Invaders-master/src/com/zetcode/sprite/Player.java
+++ Java-Space-Invaders-master/src/com/zetcode/sprite/Player.java
@@ -29,10 +29,10 @@ public class Player extends Sprite implements KeyEventDispenseChain {
         width = ii.getImage().getWidth(null);
         setImage(ii.getImage());
 
-        int START_X = 270;
+        int START_X = Commons.BOARD_WIDTH/2;
         setX(START_X);
 
-        int START_Y = 280;
+        int START_Y = Commons.BOARD_HEIGHT-62;
         setY(START_Y);
     }
 
