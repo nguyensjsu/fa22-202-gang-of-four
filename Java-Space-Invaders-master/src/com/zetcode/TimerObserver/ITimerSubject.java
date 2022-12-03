aa59dc2eafe9ad73f7b9fe5f349de2c5311b6161
rhasbinisjsu <98677825+rhasbinisjsu@users.noreply.github.com>
2022-12-02 19:21:43 -0800

    changes to timer observer

--- Java-Space-Invaders-master/src/com/zetcode/TimerObserver/ITimerSubject.java
+++ Java-Space-Invaders-master/src/com/zetcode/TimerObserver/ITimerSubject.java
@@ -1,12 +1,8 @@
 package com.zetcode.TimerObserver;
 
-import java.awt.Graphics ;
-
 public interface ITimerSubject {
     
     void attach( ITimerObserver obj ) ;
     void detach( ITimerObserver obj ) ;
     void notifyObservers() ;
-    void notifyObservers( Graphics g ) ;
-
 }
--- Java-Space-Invaders-master/src/com/zetcode/TimerObserver/TimerSubject.java
+++ Java-Space-Invaders-master/src/com/zetcode/TimerObserver/TimerSubject.java
@@ -1,48 +1,39 @@
 package com.zetcode.TimerObserver;
 
 import java.util.ArrayList;
-import java.util.List ;
-import java.awt.Graphics;
+
 
 public class TimerSubject implements ITimerSubject{
-    
-    private List<ITimerObserver> observers ;
-    private long time ;
 
-    public TimerSubject() {
-        this.observers = new ArrayList<>();
+    //===========
+    private long time;
+    private ArrayList<ITimerObserver> observers = new ArrayList<ITimerObserver>();
+
+    public TimerSubject(long inTime){
+        time = inTime;
     }
 
-    public void attach( ITimerObserver obj ) {
+    public long getState(){
+        return this.time;
+    }
+
+    @Override
+    public void attach(ITimerObserver obj) {
         observers.add(obj);
     }
 
-    public void detach( ITimerObserver obj ) {
-        observers.remove(obj) ;
+    @Override
+    public void detach(ITimerObserver obj) {
+        observers.remove(obj);
     }
 
     @Override
     public void notifyObservers() {
         for (ITimerObserver obj : observers) {
-            obj.update(time) ;
+            obj.update();
         }
     }
 
-    @Override
-    public void notifyObservers( Graphics g ) {
-        for (ITimerObserver obj : observers ) {
-            obj.update(time, g ) ;
-        }
-    }
-
-    public long getTime( long time ) {
-        this.time = time ;
-        return this.time ;
-    }
-
-    public void drawTime( long currTime, Graphics g ) {
-        notifyObservers(g);
-    }
 
 
 }
